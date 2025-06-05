b5a5fcf697e351618277a111d1acb1cb9ecf1389
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-05 23:36:27 +0900

    added docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn -b 0.0.0.0 -p 8000 config.wsgi:application
+gunicorn -b 0.0.0.0 -p 8000 vchdbuxoroback.wsgi:application
