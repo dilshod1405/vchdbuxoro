d459ffeb4b441e140a1396e0531491bef8639dd4
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-06 21:54:24 +0900

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn -b 0.0.0.0 -p 8000 vchdbuxoroback.wsgi:application
+gunicorn --bind 0.0.0.0:8000 vchdbuxoroback.wsgi:application
